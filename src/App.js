ff0d123bb13b8d696a1ee4abad4ba8b00092292d
Santiago Martin <santin.martin@gmail.com>
2021-01-27 20:53:49 -0300

    sth sth sth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import logo from './sslogo.svg';
 import './App.css';
 
+import { Layout, Button, Carousel } from 'antd';
+import 'antd/dist/antd.css';
+
+const { Header, Content } = Layout;
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="wipText">
-          Since late 2020 a square symmetry is being developed in the wild...
-        </p>
-      </header>
-    </div>
+    <Layout>
+      <Header>
+        <Button>ABOUT US</Button>
+        <Button>SERVICES</Button>
+        <Button>CONTACT</Button>
+      </Header>
+      <Content>
+        <Carousel style={{ height: '90vh'}}dotPosition="right">
+          <div className='App-content'>
+              <img src={logo} className="App-logo" alt="logo" />
+              <p className="wipText">
+                Since late 2020 a square symmetry is being developed in the wild...
+              </p>
+          </div>
+          <div>Test 2</div>
+        </Carousel>
+      </Content>
+    </Layout>
   );
 }
 
