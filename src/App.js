28254663e9a1fa159ce18cfee97d024ebabf7905
Santiago Martin <santin.martin@gmail.com>
2021-01-28 14:44:40 -0300

    initial layout

--- src/App.js
+++ src/App.js
@@ -1,30 +1,78 @@
-import logo from './sslogo.svg';
-import './App.css';
+import logo from './images/ss-logo.svg';
+import smallSSLogo from './images/ss-small-logo.svg';
 
-import { Layout, Button, Carousel } from 'antd';
+import { Layout, Button } from 'antd';
 import 'antd/dist/antd.css';
 
 const { Header, Content } = Layout;
 
+const blackButtonStyle = {
+  float: 'right',
+  margin: '15px 15px 15px 0px',
+  height: '34px',
+  width: '128px',
+  textAlign: 'left',
+  backgroundColor: '#000000',
+  color: 'white',
+  verticalAlign: 'middle',
+  fontWeight: 'bold',
+  fontSize: '12px',
+  padding: '5px',
+  borderColor: '#000000',
+};
+
+const cyanButtonStyle = {
+  float: 'right',
+  margin: '15px 15px 15px 0px',
+  height: '34px',
+  width: '128px',
+  backgroundColor: '#30D6E0',
+  borderColor: '#30D6E0',
+  color: 'white',
+  textAlign: 'left',
+  fontSize: '12px',
+  fontWeight: 'bold',
+  verticalAlign: 'middle',
+  padding: '5px',
+};
+
+const headerStyle = {
+  backgroundColor: '#373737',
+  paddingRight: '15px',
+  paddingLeft: '15px',
+};
+
+const contentStyle = {
+  backgroundColor: '#000000',
+};
+
 function App() {
   return (
     <Layout>
-      <Header>
-        <Button>ABOUT US</Button>
-        <Button>SERVICES</Button>
-        <Button>CONTACT</Button>
+      <Header style={ headerStyle } >
+        <div style={{
+          position: 'relative',
+          float: 'left',
+        }}>
+          <img 
+            src={ smallSSLogo  } 
+            alt="Square Symmetry small logo"
+            height='34px'
+          />
+        </div>
+        <div style={{ 
+          position: 'relative',
+        }}>
+          <Button style={ cyanButtonStyle }>CONTACT</Button>
+          <Button style={ blackButtonStyle }>SERVICES</Button>
+          <Button style={ cyanButtonStyle }>ABOUT US</Button>
+        </div>
       </Header>
-      <Content>
-        <Carousel style={{ height: '90vh'}}dotPosition="right">
-          <div className='App-content'>
-              <img src={logo} className="App-logo" alt="logo" />
-              <p className="wipText">
-                Since late 2020 a square symmetry is being developed in the wild...
-              </p>
-          </div>
-          <div>Test 2</div>
-        </Carousel>
-      </Content>
+      {/*<Content>
+        <div style={ contentStyle }>
+
+        </div>
+      </Content> */}
     </Layout>
   );
 }
