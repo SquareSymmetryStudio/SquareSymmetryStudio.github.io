ed3657f229df44914a179eb7cf2e7273a8237487
os-studio <neonuts@gmail.com>
2021-02-02 08:39:02 -0300

    update home screen

--- src/components/SquareSymmetry/Home/Home.js
+++ src/components/SquareSymmetry/Home/Home.js
@@ -11,13 +11,11 @@ function Home() {
     return (
         <>
             <Row className='logo__row'>
-                <Col span={24}>
                     <img 
                         className='logo'
                         src={ssTextLogo} 
                         alt='ss-text-logo'
-                    />                   
-                </Col>
+                    />     
             </Row>
         </>
     );
