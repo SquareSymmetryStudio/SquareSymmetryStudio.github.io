4d6d28a17792d0410f43310178988756ac15950c
os-studio <neonuts@gmail.com>
2021-02-05 12:36:36 -0300

    Updated services descriptions

--- src/components/SquareSymmetry/Services/Services.js
+++ src/components/SquareSymmetry/Services/Services.js
@@ -23,13 +23,14 @@ function Services() {
                     <Row> 
                         <Col span={24}>
                             <Title className='sub-title'>
-                                <span className='cyan'>Unreal Engine App Development</span>
+                                <span className='cyan'>Unreal Engine 4 Development</span>
                             </Title>
                         </Col>  
                     </Row>
                     <Row> 
                         <Col span={24}>
-                            <p className='white'>Description 1</p>
+                            <p className='white'>We provide development of custom software using Unreal Engine 4
+                            for VR experiences, Art installations, Interactive experiences<br></br> and pre-visualization for video production.</p>
                         </Col>
                     </Row>
                     <Row> 
@@ -41,7 +42,8 @@ function Services() {
                     </Row>
                     <Row> 
                         <Col span={24}>
-                            <p className='white'>Description 2</p>
+                            <p className='white'>We can design, create and animate your Mobile App or Website User Interface, to achieve
+                            <br></br> the best experience and communication with your users</p>
                         </Col>
                     </Row>
                     <Row> 
@@ -53,7 +55,8 @@ function Services() {
                     </Row>
                     <Row> 
                         <Col span={24}>
-                            <p className='white'>Description 3</p>
+                            <p className='white'> We can create a fully functional Website and Web App tailored to show the best of what you offer to your potential clients.
+                            <br></br> We use technologies like React, Electron and Three.js</p>
                         </Col>
                     </Row>
                 </Col>
