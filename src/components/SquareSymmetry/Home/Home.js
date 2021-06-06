efa056ec2b235236bace10012ca7902b397599b8
Nuraldin <santin.martin@gmail.com>
2021-06-05 22:39:18 -0300

    tested adding three.js

--- src/components/SquareSymmetry/Home/Home.js
+++ src/components/SquareSymmetry/Home/Home.js
@@ -1,20 +1,29 @@
 import { 
     Col,
     Image,
+    Row,
 } from 'antd';
+
 
 import './Home.css'
 
 import ssTextLogo from '../../../images/ss-text-logo.svg';
+import Design from '../Design';
 
 function Home() {
     return (
         <Col className='logo' span={24}>
-            <Image 
-                src={ssTextLogo} 
-                alt='ss-text-logo'
-                preview={false}
-            /> 
+            <Row>
+                <Image 
+                    src={ssTextLogo} 
+                    alt='ss-text-logo'
+                    preview={false}
+                /> 
+            </Row>
+            <Row>
+                <div id="myThree"></div>
+                <Design ></Design>
+            </Row>
         </Col>
     );
 };
